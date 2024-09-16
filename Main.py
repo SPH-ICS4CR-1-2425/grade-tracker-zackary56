cb47444a15808e9c4e94559df28c56e0944c4c58
zackary56 <114748492+zackary56@users.noreply.github.com>
2024-09-16 11:11:12 -0400

    workibg

--- Main.py
+++ Main.py
@@ -1,15 +1,22 @@
 # Grade Tracker
-# Author:
+#zackaryizacc
 # Grade: ICS4C
 
 #Main Menu function
 def menu():
     pass
+print("[1]welcome")
+print("[2]pick a class")
+print("[0]exit")
 
 #Add a class function
 def addClass():
     pass
 
+print("[1] m's s class")
+print("[2] ms e class")
+print("[3] ms b class")
+print("[0]exit")
 #Add a grade to a particular clss
 def addGrade():
     pass
@@ -29,4 +36,6 @@ def showAverage():
 #Show the overall average
 def overallAverage():
     pass
+
+
 
